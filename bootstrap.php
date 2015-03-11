11e8da7949f7e4a430aed2665937224d24489129
zhaoqing <alex.q.chao@gmail.com>
2015-03-11 20:16:26 +0800

    added composer.json; change bootstrap to use autoload.php provided by composer; added a testcase for Etag\Compiler\Token and refactored one method of the class; removed require_onces in the testcases; added phpunit.xml.dist; added .gitignore

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: bigbigant
+ * Date: Mar 11 2015
+ * 
+ * This file is only for the examples and tests.
+ * In a production environment, to use a global bootstrap to register autoloader is recommended. 
+ */
+
+require_once __DIR__.'/vendor/autoload.php';
