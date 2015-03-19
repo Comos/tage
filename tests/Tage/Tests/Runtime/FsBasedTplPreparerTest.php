bc71745c12703e2e382ef6ed23d64655d4cf0f54
zhaoqing <alex.q.chao@gmail.com>
2015-03-19 11:34:49 +0800

    completed the FsBasedTplPreparerTest. fixed the class declaration in Abstract Template.php

--- tests/Tage/Tests/Runtime/FsBasedTplPreparerTest.php
+++ tests/Tage/Tests/Runtime/FsBasedTplPreparerTest.php
@@ -20,6 +20,34 @@ class FsBasedTplPreparerTest extends \PHPUnit_Framework_TestCase
         $this->compiledTplDir = __DIR__ . '/_compiledTpl';
         $this->greetingTpl = $this->tplDir . '/greeting.phtml';
     }
+    
+    protected function tearDown()
+    {
+        /**
+         * @var \SplFileInfo[]
+         */
+        $di = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->compiledTplDir));
+        $dirs = [];
+        $files = [];
+        foreach($di as $file)
+        {
+            if (strlen($file->getRealPath()) <= strlen($this->compiledTplDir) || strpos($file->getRealPath(), 'README.md'))
+            {
+                continue;
+            }
+            if ($file->isDir())
+            {
+                $dirs[] = $file->getRealPath();
+            } else {
+                $files[] = $file->getRealPath();
+            }
+        }
+        foreach (array_merge($files, $dirs) as $f)
+        {
+            \unlink($file);
+        }
+        parent::tearDown();
+    }
 
     public function test__construct()
     {
@@ -69,15 +97,25 @@ class FsBasedTplPreparerTest extends \PHPUnit_Framework_TestCase
     
     public function testPrepare()
     {
-        $this->markTestIncomplete();
+        $sourceFile = $this->tplDir.'/greeting.phtml';
+        $compiled = $this->tplDir.'/greeting.phtml.compiled.php';
         $compiler = $this->mockOfCompiler();
+        $compiler->expects($this->exactly(1))
+            ->method('compile')
+            ->with(file_get_contents($sourceFile))
+            ->willReturn(file_get_contents($compiled));
+        
         $options = [
             'tplDir' => $this->tplDir,
             'compiledTplDir' => $this->compiledTplDir,
         ];
+        
         $preparer = new FsBasedTplPreparer($options, $compiler);
         $view = $preparer->prepare('greeting.phtml');
         $this->assertInstanceOf('Tage\Runtime\AbstractTemplate', $view);
+        
+        $view1 = $preparer->prepare('greeting.phtml');
+        $this->assertInstanceOf('Tage\Runtime\AbstractTemplate', $view1);
     }
 
     /**
---
+++ tests/Tage/Tests/Runtime/_tpl/greeting.phtml.compiled.php
@@ -0,0 +1,12 @@
+<?php
+class _Tage_Compiled_Tpl_9da120710f4bbad01e45da84fb90b9dd extends Tage\Runtime\AbstractTemplate
+{
+    public function run($vars)
+    {extract($vars);
+?><html>
+    <body>
+        <h1>Hello <?php echo $who;?></h1>
+    </body>
+</html><?php
+    }
+}
