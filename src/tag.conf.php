4e826b271eaa3057785614534a7e4a182405775d
bigbigant <alex.q.chao@gmail.com>
2015-04-24 13:34:08 +0800

    add tag.conf.php

---
+++ src/tag.conf.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+    't' => array(
+       'class'=>'Comos\\Tage\\Compiler\\Tags\\T',
+    ),
+    'if'=>array(
+        'class'=>'Comos\\Tage\\Compiler\\Tags\\If',
+    )
+);
+
+// class CommonTag
+// {
+//     public $closeTagName='';//false 表示无需关闭
+//     //option requried
+//     /**
+//      *  'key'=>array('type'=>'option',)
+//      * @var unknown
+//      */
+//     public $params=array();//参数列表
+// }
+
+// class TTag extends CommanTag
+// {
+    
+    
+// }
